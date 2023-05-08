5049473f0bc67a36cf5f1ae934eeffc4eb3c8052
Hannes Knutsson <hannes.knutsson@protonmail.com>
2023-05-08 22:35:50 +0200

    generate model and client

--- src/main/java/com/github/hannesknutsson/zappbrannigan/ZappBranniganApplication.java
+++ src/main/java/com/github/hannesknutsson/zappbrannigan/ZappBranniganApplication.java
@@ -1,13 +1,14 @@
 package com.github.hannesknutsson.zappbrannigan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableScheduling
 @SpringBootApplication
 public class ZappBranniganApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZappBranniganApplication.class, args);
 	}
-
 }
