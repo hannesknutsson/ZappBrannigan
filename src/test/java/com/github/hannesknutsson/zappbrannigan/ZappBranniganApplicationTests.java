79be98e0d6f0e58375d9dfc01edb76fad0461e57
Hannes Knutsson <hannes.knutsson@protonmail.com>
2023-05-08 19:37:24 +0200

    spring initializer

---
+++ src/test/java/com/github/hannesknutsson/zappbrannigan/ZappBranniganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hannesknutsson.zappbrannigan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZappBranniganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
